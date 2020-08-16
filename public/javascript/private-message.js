1c44de6b70eebe7d400d228609964ad9ceb69017
Wanglala <sue4100035045@gmail.com>
2020-08-16 16:01:16 +0800

    feat: privateMsg scrollBottom

--- public/javascript/private-message.js
+++ public/javascript/private-message.js
@@ -43,7 +43,7 @@ $(function () {
       }
     }
 
-    chatContent.scrollTop = chatContent.scrollHeight
+    mailContent.scrollTop = mailContent.scrollHeight
   })
 
   socket.on('sendPrivate', data => {
